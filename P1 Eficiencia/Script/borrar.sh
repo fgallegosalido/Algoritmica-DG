1ab7d3b758949b7f485da52873ab8050ae257e5e
Rubenmp <srmorales@correo.ugr.es>
2016-03-10 23:09:45 +0100

    Arreglando script

--- P1 Eficiencia/Script/borrar.sh
+++ P1 Eficiencia/Script/borrar.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-if [ $# -ne 2 ] 
-then 
-    echo "Uso: $0 <nombre>"
-    exit(1)
-fi
 
-rm *~
-rm ./gnuplot/*~
-rm ../src/*~
-rm ../Graficas/Burbuja/*$1*
-rm ../Graficas/Burbuja/Datos/*$1*
-rm ../Graficas/Fibonacci/*$1*
-rm ../Graficas/Fibonacci/Datos/*$1*
-rm ../Graficas/Floyd/*$1*
-rm ../Graficas/Floyd/Datos/*$1*
-rm ../Graficas/Hanoi/*$1*
-rm ../Graficas/Hanoi/Datos/*$1*
-rm ../Graficas/Heapsort/*$1*
-rm ../Graficas/Heapsort/Datos/*$1*
-rm ../Graficas/Insercion/*$1*
-rm ../Graficas/Insercion/Datos/*$1*
-rm ../Graficas/Mergesort/*$1*
-rm ../Graficas/Mergesort/Datos/*$1*
-rm ../Graficas/Quicksort/*$1*
-rm ../Graficas/Quicksort/Datos/*$1*
-rm ../Graficas/Seleccion/*$1*
-rm ../Graficas/Seleccion/Datos/*$1*
+
+rm -f *~
+rm -f ./gnuplot/*~
+rm -f ../src/*~
+rm -f ../Graficas/Burbuja/*$1*
+rm -f ../Graficas/Burbuja/Datos/*$1*
+rm -f ../Graficas/Fibonacci/*$1*
+rm -f ../Graficas/Fibonacci/Datos/*$1*
+rm -f ../Graficas/Floyd/*$1*
+rm -f ../Graficas/Floyd/Datos/*$1*
+rm -f ../Graficas/Hanoi/*$1*
+rm -f ../Graficas/Hanoi/Datos/*$1*
+rm -f ../Graficas/Heapsort/*$1*
+rm -f ../Graficas/Heapsort/Datos/*$1*
+rm -f ../Graficas/Insercion/*$1*
+rm -f ../Graficas/Insercion/Datos/*$1*
+rm -f ../Graficas/Mergesort/*$1*
+rm -f ../Graficas/Mergesort/Datos/*$1*
+rm -f ../Graficas/Quicksort/*$1*
+rm -f ../Graficas/Quicksort/Datos/*$1*
+rm -f ../Graficas/Seleccion/*$1*
+rm -f ../Graficas/Seleccion/Datos/*$1*
 
 
