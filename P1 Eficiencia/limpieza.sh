ee4fbb69f96b6c437d51a0ca6accff4b9b9b1bcd
Rubenmp <srmorales@correo.ugr.es>
2016-03-16 18:54:14 +0100

    Presentación en beamer casi terminada

---
+++ P1 Eficiencia/limpieza.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+find . -regex ".*~" -exec rm -r {} \;
+rm -f ./Presentacion/beamer.aux
+rm -f ./Presentacion/beamer.log
+rm -f ./Presentacion/beamer.nav
+rm -f ./Presentacion/beamer.out
+rm -f ./Presentacion/beamer.vrb
+rm -f ./Presentacion/beamer.snm
+rm -f ./Presentacion/beamer.toc
+rm -f ./Presentacion/beamer.synctex.gz
+rm -f ./Presentacion/memoria.aux
+rm -f ./Presentacion/memoria.log
+rm -f ./Presentacion/memoria.out
+rm -f ./Presentacion/memoria.synctex.gz
+rm -f ./Presentacion/memoria.toc
+
