b6a5212c2e5c1f4c1776da5350919de4826f3c1c
fgallegosalido <fgallegosalido@gmail.com>
2016-04-07 13:27:07 +0200

    Modificación visual

--- P2 DyV/src/genera-mezclavectores.cpp
+++ P2 DyV/src/genera-mezclavectores.cpp
@@ -7,66 +7,67 @@ using namespace std;
 //#include <algorithm>
 //#include <vector>
 
-//generador de ejemplor para el problema de mezcla de k vectores ordenados. Para obtener k vectores ordenados de forma creciente cada uno con n elementos, genera un vector de tamaño k*n con todos los enteros entre 0 y kn-1 ordenados. Se lanzan entonces k iteraciones de un algoritmo de muestreo aleatorio de tamaño n para obtener los k vectores. Están ordeados porque el algoritmo de muestreo mantiene el orden
+//generador de ejemplor para el problema de mezcla de k vectores ordenados.
+//Para obtener k vectores ordenados de forma creciente cada uno con n elementos,
+//genera un vector de tamaÃ±o k*n con todos los enteros entre 0 y kn-1 ordenados.
+//Se lanzan entonces k iteraciones de un algoritmo de muestreo aleatorio de
+//tamaÃ±o n para obtener los k vectores. EstÃ¡n ordeados porque el algoritmo de muestreo mantiene el orden
 
-double uniforme() //Genera un número uniformemente distribuido en el
-                  //intervalo [0,1) a partir de uno de los generadores
-                  //disponibles en C. 
-{
- int t = rand();
- double f = ((double)RAND_MAX+1.0);
- return (double)t/f;
+double uniforme(){ //Genera un nÃºmero uniformemente distribuido en el
+                   //intervalo [0,1) a partir de uno de los generadores
+                   //disponibles en C.
+  int t = rand();
+  double f = ((double)RAND_MAX+1.0);
+  return (double)t/f;
 }
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]){
 
-  if (argc != 3)
-    {
-      cerr << "Formato " << argv[0] << " <num_elem>" << " <num_vect>" << endl;
-      return -1;
-    }
+  if (argc != 3){
+    cerr << "Formato " << argv[0] << " <num_elem>" << " <num_vect>" << endl;
+    return -1;
+  }
 
   int n = atoi(argv[1]);
   int k=atoi(argv[2]);
 
 
-int **T;
-T =  new int * [k];
+  int **T;
+  T =  new int * [k];
   assert(T);
 
- for (int i = 0; i < k; i++)
-      T[i]= new int [n];
+  for (int i = 0; i < k; i++)
+    T[i]= new int [n];
 
-int N=k*n;
-int * aux = new int[N];
+  int N=k*n;
+  int * aux = new int[N];
   assert(aux);
 
-srand(time(0));
-//genero todos los enteros entre 0 y k*n-1
-for (int j=0; j<N; j++) aux[j]=j;
+  srand(time(0));
+  //genero todos los enteros entre 0 y k*n-1
+  for (int j=0; j<N; j++) aux[j]=j;
 
-//para cada uno de los k vectores se lanza el algoritmo S (sampling) de Knuth
-for (int i=0; i<k; i++) {
-   int t=0;
-   int m=0;
-   while (m<n) {
-    double u=uniforme();
-    if ((N-t)*u >= (n-m)) t++;
-    else {
+  //para cada uno de los k vectores se lanza el algoritmo S (sampling) de Knuth
+  for (int i=0; i<k; i++) {
+    int t=0;
+    int m=0;
+    while (m<n) {
+      double u=uniforme();
+
+      if ((N-t)*u >= (n-m)) t++;
+      else {
         T[i][m]=aux[t];
-	t++;
-	m++;
+        t++;
+        m++;
+      }
     }
   }
+
+  delete [] aux;
+
+  for (int i=0; i<k; i++) {
+    for (int j=0; j<n; j++)
+      cout << T[i][j] << " ";
+    cout << " " << endl;
+  }
 }
-
-delete [] aux;
-
-for (int i=0; i<k; i++) {
- for (int j=0; j<n; j++)
-   cout << T[i][j] << " ";
- cout << " " << endl;
-}
-
-} 
