151336a8732634ab4aee6823668fb5a7eacca32a
Rubenmp <srmorales@correo.ugr.es>
2016-04-11 23:43:13 +0200

    problema, el fuerza bruta es mucho mejor que el dyv

---
+++ P2 DyV/Obligatorio/Script/script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "Uso: $0 <nombre>"
+    exit 1
+fi
+
+mkdir ../Graficas 2> /dev/null
+mkdir ../Datos 2> /dev/null
+
+# Fuerza bruta
+g++ -std=c++11 -D GP_OUT ../src/fuerza_bruta.cpp
+nelementos=10
+echo "" > datos.dat
+while [ $nelementos -lt 5000 ]; do
+    ./a.out $nelementos >> datos.dat
+    let nelementos=nelementos+10
+done
+gnuplot ./gnuplot/fuerza_bruta.gp
+
+mv grafica.png ../Graficas/fuerza_bruta_$1.png
+mv datos.dat ../Datos/fuerza_bruta_$1.dat
+mv fit.log ../Datos/fit_fuerza_bruta_$1.log
+echo "Fuerza bruta completado"
+
+# DyV
+g++ -std=c++11 -D GP_OUT ../src/dyv.cpp
+nelementos=10
+echo "" > datos.dat
+while [ $nelementos -lt 5000 ]; do
+    ./a.out $nelementos >> datos.dat
+    let nelementos=nelementos+10
+done
+gnuplot ./gnuplot/dyv.gp
+
+mv grafica.png ../Graficas/dyv_$1.png
+mv datos.dat ../Datos/dyv_$1.dat
+mv fit.log ../Datos/fit_dyv_$1.log
+echo "DyV completado"
