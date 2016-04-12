e8491f7d2e43f35637ae66d11f167c73977fc491
Rubenmp <srmorales@correo.ugr.es>
2016-04-12 17:51:12 +0200

    Generando los datos del obligatorio(mezcla)

--- P2 DyV/Obligatorio/Script/script.sh
+++ P2 DyV/Obligatorio/Script/script.sh
@@ -12,23 +12,33 @@ mkdir ../Datos 2> /dev/null
 # Fuerza bruta
 g++ -std=c++11 ../src/mezcla.cpp
 
-echo "hola\n"
+#nelementos=10
+#while [ $nelementos -lt 1500 ]; do
+#    ./a.out $nelementos 200 3
+#    let nelementos=nelementos+50
+#done
+
+nelementos=10
 nvectores=10
-echo "" > datos.dat
-while [ $nvectores -lt 1500 ]; do
-    ./a.out 200 $nvectores "kvectores" >> datos.dat
-    let nvectores=nvectores+50
+while [ $nelementos -lt 1000 ]; do
+   while [ $nvectores -lt 1000 ]; do
+       ./a.out $nelementos $nvectores 1
+       let nvectores=nvectores+100
+   done
+    let nelementos=nelementos+100
 done
-gnuplot ./gnuplot/fuerza_bruta.gp
-gnuplot ./gnuplot/dyv.gp
-gnuplot ./gnuplot/comparativa.gp
 
-mv grafica_f.png ../Graficas/fuerza_bruta_kvectores_$1.png
-mv grafica_d.png ../Graficas/dyv_kvectores_$1.png
-mv comparativa.png ../Graficas/comparativa_kvectores_$1.png
 
-mv fuerza_bruta.dat ../Datos/fuerza_bruta_kvectores_$1.dat
-mv dyv.dat ../Datos/dyv_kvectores_$1.dat
+#gnuplot ./gnuplot/fuerza_bruta.gp
+#gnuplot ./gnuplot/dyv.gp
+#gnuplot ./gnuplot/comparativa.gp
 
-mv fit.log /dev/null
+#mv grafica_f.png ../Graficas/fuerza_bruta_nelementos_$1.png
+#mv grafica_d.png ../Graficas/dyv_nelementos_$1.png
+#mv comparativa.png ../Graficas/comparativa_nelementos_$1.png
+
+mv fuerza_bruta.dat ../Datos/fuerza_bruta_3d_$1.dat
+mv dyv.dat ../Datos/dyv_3d_$1.dat
+
+rm fit.log
 echo "Terminado"
