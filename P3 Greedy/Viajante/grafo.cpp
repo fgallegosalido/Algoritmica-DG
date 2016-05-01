a851aa5c9c889c1157ceb6a94a0f969b6e03b624
Rubén Morales Pérez <srmorales@correo.ugr.es>
2016-05-01 12:44:53 +0200

    Añadiendo main

--- P3 Greedy/Viajante/grafo.cpp
+++ P3 Greedy/Viajante/grafo.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 
@@ -142,8 +146,87 @@ class Graph{
 
          return neighbours;
       }
+      
+      int* minimizingEdges(){
+          return NULL;
+      }
 };
 
-int main(){
-   int x = 5;
+Point * readPoints(std::ifstream &in, int &size){
+  char aux;
+  int dim, x, y;
+  Point* points = NULL;
+  while( in.peek() < '0' || in.peek() > '9' ) // Buscar dimensión
+    in >> aux;
+  in >> dim;
+  
+  if (dim > 0){
+      size = dim;
+      points = new Point[size];
+      for (int i=0; i<size; ++i){
+        in >> dim >> x >> y;
+        points[i].setXY(x, y);      
+      }
+  }
+  
+  return points;
 }
+    
+int main(int argc, char* argv[]){
+  bool exit = false;
+  if (argc != 4){
+    cerr << "Formato: " << argv[0] << " <fichero.dat>" << " <modo>" << " <num_ciudades>" << endl;
+    cerr << "Modo:\n\t1 --> Vecino más cercano\n\t 2 --> Inserción\n\t3 --> Minimizando aristas" << endl;
+    return -1;
+  }
+  
+  ifstream in(argv[1]);
+  int mode = atoi(argv[2]);
+  int cities = atoi(argv[3]);
+  if (!in){
+    cerr << "No se puede abri el fichero " << argv[1] << " para lectura." << endl;  
+    exit = true;
+  }
+  if ( mode > 3 || mode < 1 ){
+    cerr << "Modo incorrecto" << endl;    
+    cerr << "Modo:\n\t1 --> Vecino más cercano\n\t 2 --> Inserción\n\t3 --> Minimizando aristas" << endl;
+    exit = true;
+  }
+  if (cities < 3 || cities > 300){
+    cerr << "El número de ciudades debe estar entre 3 y 300 inclusive." << endl;
+    exit = true;
+  }
+  
+  if (exit)
+    return -1;
+
+  srandom(time(0));
+  int size, node;
+  int* order;
+  Point* points = readPoints(in, size);
+  Graph graph(points, size);
+  node = 0;
+
+  chrono::high_resolution_clock::time_point tbefore, tafter;
+  chrono::duration<double> duration;
+  
+  tbefore = chrono::high_resolution_clock::now();
+  if (mode == 1)
+    order = graph.nearestNeighbour(node);
+  else if (mode == 2)
+    order = NULL;
+    //order = graph.insertion_____();
+  else // mode == 3
+    order = graph.minimizingEdges();
+  tafter = chrono::high_resolution_clock::now();
+  duration = chrono::duration_cast<chrono::duration<double>>(tafter - tbefore);
+  
+  ofstream solution("order.dat");
+  for (int i=0; i<size; ++i)
+    solution << order[i] << graph.getPoint(order[i]).getX() << graph.getPoint(order[i]).getY() << endl;    
+
+
+}
+  
+
+  
